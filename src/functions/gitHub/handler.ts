5dcfa16bc7227a0534c941366c56e55825270405
JPBM135 <jpedrobm0@gmail.com>
2022-10-08 11:39:49 -0300

    chore: show the right line when limited Co-authored-by: Souji <timoqueezle@gmail.com>

--- src/functions/gitHub/handler.ts
+++ src/functions/gitHub/handler.ts
@@ -1,3 +1,4 @@
+import { ellipsis } from "@yuudachi/framework";
 import type { VoiceChannel } from "discord.js";
 import {
 	type AnyThreadChannel,
@@ -10,8 +11,6 @@ import {
 } from "discord.js";
 import kleur from "kleur";
 import { request } from "undici";
-import { ellipsis } from "../../util/ellipsis.js";
-import { convertUrlToRawUrl } from "../../util/gitHub.js";
 
 // eslint-disable-next-line unicorn/no-unsafe-regex
 const GitHubUrlRegex = /https:\/\/github.com\/(?<org>.+?)\/(?<repo>.+?)\/blob(?<path>\/[^\n#]+)(?<opts>.+)?/g;
@@ -19,6 +18,10 @@ const GitHubUrlRegex = /https:\/\/github.com\/(?<org>.+?)\/(?<repo>.+?)\/blob(?<
 const GitHubUrlLinesRegex = /^#L(?<start>\d+)(?:-L(?<end>\d+))?/;
 
 const SAFE_BOUNDARY = 10;
+
+function convertUrlToRawUrl(url: string) {
+	return url.replace("github.com", "raw.githubusercontent.com").replace("/blob", "");
+}
 
 function resolveEndLine(startLine: number, endLine: number, isOnThread: boolean) {
 	if (Number.isNaN(endLine)) {
@@ -76,9 +79,9 @@ export async function handleGithubUrls(message: Message<true>) {
 		const lang = path!.split(".").pop() ?? "ansi";
 
 		const content = [
-			`${end ? `Lines ${inlineCode(start!)} to ${inlineCode(end)}` : `Line ${inlineCode(start!)}`} of ${italic(
-				`${org}/${repo}${path}`,
-			)} ${isOnThread && delta > 10 ? "(Limited to 10 lines)" : ""}`,
+			`${
+				end ? `Lines ${inlineCode(start!)} to ${inlineCode(String(endLine))}` : `Line ${inlineCode(start!)}`
+			} of ${italic(`${org}/${repo}${path}`)} ${isOnThread && delta > 10 ? "(Limited to 10 lines)" : ""}`,
 		];
 
 		const maxLength = isOnThread ? 500 : 2_000 - (content[0]!.length + SAFE_BOUNDARY + lang.length);
