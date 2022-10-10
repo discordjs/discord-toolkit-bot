14451961e07fd7c88a60cab1843411fcda6ce603
JPBM135 <jpedrobm0@gmail.com>
2022-10-10 11:11:42 -0300

    revert(md escaping): don't care a

--- src/functions/github/utils.ts
+++ src/functions/github/utils.ts
@@ -1,5 +1,4 @@
 import { Buffer } from "node:buffer";
-import { escapeMarkdown } from "discord.js";
 import kleur from "kleur";
 import { GitHubUrlLinesRegex } from "./regex.js";
 
@@ -42,7 +41,7 @@ export function resolveLines(startLine: number, endLine: number, isOnThread: boo
 
 export function formatLine(line: string, start: number, end: number, index: number, ansi = false) {
 	const prefix = String(index + start).padEnd(String(end || "").length ?? 1, " ");
-	return `${ansi ? kleur.cyan(prefix) : prefix} | ${escapeMarkdown(line)}`;
+	return `${ansi ? kleur.cyan(prefix) : prefix} | ${line}`;
 }
 
 export function stringArrayLength(arr: string[]) {
