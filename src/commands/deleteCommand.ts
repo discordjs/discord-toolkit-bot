98f467e34df362dfae70a947c910a404a899d7c7
Jaworek <jaw0r3k.g@gmail.com>
2022-10-14 22:06:56 +0200

    Update deleteCommand.ts

--- src/commands/deleteCommand.ts
+++ src/commands/deleteCommand.ts
@@ -3,13 +3,13 @@ import type { InteractionParam, ArgsParam, CommandMethod } from "@yuudachi/frame
 import { PermissionFlagsBits } from "discord.js";
 import type { DeleteCommandContextCommand } from "../interactions/context/deleteCommand.js";
 
-export default class extends Command<typeof RemoveGithubThread> {
+export default class extends Command<typeof DeleteCommandContextCommand {
 	public constructor() {
 		super(["Delete Command"]);
 	}
 	public override async messageContext(
 		interaction: InteractionParam<CommandMethod.MessageContext>,
-		args: ArgsParam<typeof RemoveGithubThread>,
+		args: ArgsParam<typeof DeleteCommandContextCommand,
 	): Promise<void> {
 	   if(args.message.interaction.user.id !== interaction.user.id || !interaction.member.permissions.has(PermissionFlagsBits.ManageMessages)){
               interaction.reply({ content: "You are not author of this command.", ephemeral: true })
