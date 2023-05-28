80dd25c71f2458d6a394e9af1946ff8355e9db20
MrMythical <91077061+MrMythicalYT@users.noreply.github.com>
2023-05-28 15:46:56 -0400

    feat: format code command

--- src/commands/format.ts
+++ src/commands/format.ts
@@ -1,30 +1,54 @@
 import { Command } from "@yuudachi/framework";
 import type { InteractionParam, ArgsParam, CommandMethod } from "@yuudachi/framework/types";
-import type { FormatContextCommand } from "../interactions/context/format.js";
+import { codeBlock } from "discord.js";
 import { format } from "prettier";
-import { codeBlock } from "discord.js"
+import type { FormatContextCommand } from "../interactions/context/formatContext.js";
 
 export default class extends Command<typeof FormatContextCommand> {
 	public constructor() {
 		super(["Format code"]);
 	}
-	
+
 	public override async messageContext(
 		interaction: InteractionParam<CommandMethod.MessageContext>,
 		args: ArgsParam<typeof FormatContextCommand>,
 	): Promise<void> {
 		await interaction.deferReply({ ephemeral: true });
-		const code = format(args.message.content, { 
-				printWidth: 120,
-				useTabs: true,
-				quoteProps: "as-needed",
-				trailingComma: "all",
-				endOfLine: "lf",
-				semi: true,
-				singleQuote: true,
-				parser: "typescript",
+		if (!args.message.content) {
+			await interaction.editReply("There is no code to format here.");
+			return;
+		}
+
+		const matches = args.message.content.matchAll(/```(?<lang>\w*)\n(?<code>.+?)\n```/gs);
+		if (!matches) {
+			await interaction.editReply("There is no code to format here.");
+			return;
+		}
+
+		const codeBlocks: string[] = [];
+		for (const match of matches) {
+			try {
+				const formattedCode = format(match.groups!.code!, {
+					printWidth: 120,
+					useTabs: true,
+					quoteProps: "as-needed",
+					trailingComma: "all",
+					endOfLine: "lf",
+					semi: true,
+					singleQuote: true,
+					filepath: `code.${match.groups!.lang ?? "ts"}`,
+				});
+				codeBlocks.push(codeBlock(match.groups!.lang ?? "ts", formattedCode));
+			} catch {
+				continue;
 			}
-		)
-		await interaction.editReply({ content: codeBlock('ts', code) })	
+		}
+
+		if (!codeBlocks.length) {
+			await interaction.editReply("Could not format any codeblocks in this message.");
+			return;
+		}
+
+		await interaction.editReply({ content: codeBlocks.join("\n") });
 	}
 }
