341075bc54c9aff2f9b9ed399363342e5939b3ce
almostSouji <timoqueezle@gmail.com>
2025-07-08 12:54:45 +0200

    feat(commands): add bit field commands
    * slash cmd: permissions
    * slash cmd: gateway intents
    * message context cmd: gateway intents
    
    resolves #27

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,13 @@ import { setTimeout } from "node:timers";
 import { URL, fileURLToPath } from "node:url";
 import type { ToEventProps } from "@discordjs/core";
 import { Client, GatewayDispatchEvents, GatewayIntentBits, MessageFlags } from "@discordjs/core";
+import { inlineCode } from "@discordjs/formatters";
 import { REST } from "@discordjs/rest";
 import { WebSocketManager } from "@discordjs/ws";
 import * as TOML from "@ltd/j-toml";
 import type { GatewayMessageCreateDispatchData } from "discord-api-types/v10";
 import {
+	ApplicationCommandOptionType,
 	ApplicationCommandType,
 	ButtonStyle,
 	ChannelType,
@@ -19,6 +21,9 @@ import {
 } from "discord-api-types/v10";
 import { ComponentType, InteractionType } from "discord-api-types/v9";
 import { pino } from "pino";
+import { IntentsLookupContextCommand } from "./interactions/context/intentsLookupContext.js";
+import { BitfieldLookupCommand } from "./interactions/slash/bitfieldLookup.js";
+import { formatBits } from "./util/bits.js";
 import { ASSISTCHANNELS, SUPPORT_CHANNEL, SUPPORT_CHANNEL_VOICE } from "./util/constants.js";
 
 type AutoResponse = {
@@ -88,6 +93,13 @@ client.on(GatewayDispatchEvents.MessageCreate, async ({ data: message }) => {
 	}
 });
 
+/**
+ * Wait for message in the provided channel
+ *
+ * @param channelId - The id of the channel to listen in
+ * @param timeoutMs - The time in milliseconds to wait until the listener is aborted
+ * @returns The received message
+ */
 async function waitForMessage(channelId: string, timeoutMs?: number) {
 	return new Promise((resolve, reject) => {
 		const callback = ({ data: message }: ToEventProps<GatewayMessageCreateDispatchData>) => {
@@ -220,13 +232,81 @@ client.on(GatewayDispatchEvents.InteractionCreate, async ({ data: interaction })
 	}
 
 	if (interaction.type === InteractionType.ApplicationCommand) {
-		if (interaction.data.type === ApplicationCommandType.Message) {
-			// TODO: message context intents
+		if (
+			interaction.data.type === ApplicationCommandType.Message &&
+			interaction.data.name === IntentsLookupContextCommand.name
+		) {
+			const messages = interaction.data.resolved.messages;
+			const message = messages[interaction.data.target_id];
+
+			if (!message) {
+				logger.info("Expected to find message during context menu execution but found none.");
+				return;
+			}
+
+			const res =
+				/intents:.*?(?<bits>\d{1,10})/gi.exec(message.content) ??
+				/intents\((?<bits>\d{1,10})\)/gi.exec(message.content) ??
+				/intents.*?(?<bits>\d{1,10})/gi.exec(message.content) ??
+				/(?:^|[\s`])(?<bits>\d{1,10}?)(?:$|[\s`])/gi.exec(message.content);
+
+			if (!res) {
+				await client.api.interactions.reply(interaction.id, interaction.token, {
+					flags: MessageFlags.Ephemeral,
+					content: "Cannot find any potential Gateway Intent numerals in this message.",
+				});
+				return;
+			}
+
+			const bits = Number.parseInt(res[1]!, 10);
+			const formatted = formatBits(bits, GatewayIntentBits, "Gateway Intent");
+
+			await client.api.interactions.reply(interaction.id, interaction.token, {
+				flags: MessageFlags.IsComponentsV2 | MessageFlags.Ephemeral,
+				components: [formatted],
+			});
+			return;
 		}
 
-		if (interaction.data.type === ApplicationCommandType.ChatInput) {
-			// TODO: bitfield permissions
-			// TODO: bitfield intents
+		if (
+			interaction.data.type === ApplicationCommandType.ChatInput &&
+			interaction.data.name === BitfieldLookupCommand.name
+		) {
+			const sub = interaction.data.options?.[0];
+
+			if (sub?.type !== ApplicationCommandOptionType.Subcommand) {
+				return;
+			}
+
+			const bitsOption = sub.options?.[0];
+			if (bitsOption?.name !== "bitfield") {
+				return;
+			}
+
+			const bits = bitsOption.value;
+
+			if (typeof bits === "boolean") {
+				return;
+			}
+
+			if (Number.isNaN(Number.parseInt(String(bits), 10))) {
+				await client.api.interactions.reply(interaction.id, interaction.token, {
+					flags: MessageFlags.Ephemeral,
+					content: `Could not resolve ${inlineCode(String(bits))} to a bit field.`,
+				});
+				return;
+			}
+
+			const serializer = sub.name === "intents" ? GatewayIntentBits : PermissionFlagsBits;
+			const prefix = sub.name === "intents" ? "Gateway Intents" : "Permission";
+			const formatted = formatBits(typeof bits === "string" ? BigInt(bits) : bits, serializer, prefix);
+
+			await client.api.interactions.reply(interaction.id, interaction.token, {
+				flags: MessageFlags.IsComponentsV2 | MessageFlags.Ephemeral,
+				components: [formatted],
+			});
+
+			return;
 		}
 	}
 
--- src/util/bits.ts
+++ src/util/bits.ts
@@ -1,10 +1,18 @@
-import { codeBlock, inlineCode } from "@discordjs/formatters";
+import { HeadingLevel, codeBlock, heading, inlineCode } from "@discordjs/formatters";
 import type { APIMessageTopLevelComponent, PermissionFlagsBits, GatewayIntentBits } from "discord-api-types/v10";
 import { ComponentType } from "discord-api-types/v10";
 import kleur from "kleur";
 
 kleur.enabled = true;
 
+/**
+ * Format bitfields to a display component based on the provided serilization dictionary
+ *
+ * @param bits - The bit field to resolve
+ * @param serializer - The serialization dictionary of all possible values and their flags to use
+ * @param headingPrefix - The prefix to display in the resulting heading
+ * @returns A formatted display component representing the provided bit field
+ */
 export function formatBits<T extends bigint | number>(
 	bits: T,
 	serializer: typeof GatewayIntentBits | typeof PermissionFlagsBits,
@@ -13,12 +21,15 @@ export function formatBits<T extends bigint | number>(
 	const formattedFlags: string[] = [];
 
 	for (const [flag, bit] of Object.entries(serializer)) {
-		if (Number.isNaN(Number.parseInt(flag, 10))) {
-			const isSet = (bits && bit) === bit;
-			formattedFlags.push(
-				`${isSet ? kleur.green("[✔]") : kleur.red("[✖]")} ${flag} (${bit}) 1<<${bit.toString(2).length - 1}`,
-			);
+		if (!Number.isNaN(Number.parseInt(flag, 10))) {
+			// filter out bit -> flagname mappings in bidirectional maps
+			continue;
 		}
+
+		const isSet = (bits & bit) === bit;
+		formattedFlags.push(
+			`${isSet ? kleur.green("[✔]") : kleur.red("[✖]")} ${flag} (${bit}) 1<<${bit.toString(2).length - 1}`,
+		);
 	}
 
 	return {
@@ -27,7 +38,7 @@ export function formatBits<T extends bigint | number>(
 			{
 				type: ComponentType.TextDisplay,
 				content: [
-					`${headingPrefix} deconstruction for bitfield ${inlineCode(String(bits))}`,
+					heading(`${headingPrefix} deconstruction for bitfield ${inlineCode(String(bits))}`, HeadingLevel.Three),
 					codeBlock("ansi", formattedFlags.join("\n")),
 				].join("\n"),
 			},
